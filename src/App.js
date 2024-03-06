a116de05507550fb7fa0bd2e6ca7cec2a3bd43c7
Vishal kumar <vishalk80269@gmail.com>
2024-03-06 13:06:57 +0530

    loader add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { RiDeleteBin7Fill } from "react-icons/ri";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import AddTodo from "./Components/AddTodo";
+import Loader from "./Components/Loader";
 const App = () => {
   const {
     getAllTodo,
@@ -91,113 +92,118 @@ const App = () => {
   };
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
   return (
-    <section className="hero">
-      <div className="container">
-        <div className="row text-center">
-          <h4>WELCOME TO EARNEST FINTECH LIMITED</h4>
-          <p>Task Management System</p>
-        </div>
-        <div className="todoSingleContainer my-2">
-          <div>
-            <h6>{data?.title}</h6>
-
-            <p>{data?.description}</p>
-
-            <i>{new Date(data?.createdAt).toDateString()}</i>
-            <br />
-            <button className="btn btn-dark">
-              {data?.completed ? "Done" : "Pending"}
-            </button>
+    <>
+      {loading && <Loader />}
+      <section className="hero">
+        <div className="container">
+          <div className="row text-center">
+            <h4>WELCOME TO EARNEST FINTECH LIMITED</h4>
+            <p>Task Management System</p>
           </div>
-        </div>
+          <div className="todoSingleContainer my-2">
+            <div>
+              <h6>{data?.title}</h6>
 
-        <div className="mainTask">
-          <div className="row mainTodo my-4">
-            <div className="row searchFun m-0  g-0">
-              <div className="innerSearch m-0 p-0 g-0">
-                <div className="left">
-                  <div>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="search title.."
-                      value={checkSearch}
-                      onChange={(e) => handleSearch(e.target.value)}
-                    />
+              <p>{data?.description}</p>
+
+              <i>{new Date(data?.createdAt).toDateString()}</i>
+              <br />
+              <button className="btn btn-dark">
+                {data?.completed ? "Done" : "Pending"}
+              </button>
+            </div>
+          </div>
+
+          <div className="mainTask">
+            <div className="row mainTodo my-4">
+              <div className="row searchFun m-0  g-0">
+                <div className="innerSearch m-0 p-0 g-0">
+                  <div className="left">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="search title.."
+                        value={checkSearch}
+                        onChange={(e) => handleSearch(e.target.value)}
+                      />
+                    </div>
+                    <div>
+                      <button className="btn btn-dark" onClick={handleTodo}>
+                        Add Todo
+                      </button>
+                    </div>
                   </div>
-                  <div>
-                    <button className="btn btn-dark" onClick={handleTodo}>
-                      Add Todo
+                  <div className="right">
+                    <button className="btn btn-dark">Total: {total}</button>
+                    <button className="btn btn-danger">
+                      Pending: {pending}
                     </button>
+                    <button className="btn btn-success">Done: {done}</button>
                   </div>
-                </div>
-                <div className="right">
-                  <button className="btn btn-dark">Total: {total}</button>
-                  <button className="btn btn-danger">Pending: {pending}</button>
-                  <button className="btn btn-success">Done: {done}</button>
                 </div>
               </div>
-            </div>
-            <div className="row m-0 g-0 ">
-              <table class="table m-0 mainTable">
-                <thead>
-                  <tr>
-                    <th scope="col">S.no</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {filterTask?.map((t, i) => (
-                    <tr className="tableDataSetCursor" key={t._id}>
-                      <th scope="row">{i + 1}</th>
-                      <td onClick={() => handleDetails(t)}>{t?.title}</td>
-                      <td onClick={() => handleDetails(t)}>
-                        {t?.description.substring(0, 30)} read more..
-                      </td>
-                      <td>{new Date(t?.createdAt).toDateString()}</td>
-                      <td>
-                        {" "}
-                        <Checkbox
-                          {...label}
-                          checked={t.completed}
-                          value={t.completed}
-                          color="success"
-                          onClick={() => handleUpdateStatus(t)}
-                        />
-                      </td>
-                      <td>
-                        <button
-                          className="btn btn-danger"
-                          onClick={() => handleDelete(t._id)}
-                        >
-                          Delete
-                        </button>
-                        <button
-                          className="btn btn-success mx-2"
-                          onClick={() => handleUpdate(t)}
-                        >
-                          Update
-                        </button>
-                      </td>
+              <div className="row m-0 g-0 ">
+                <table class="table m-0 mainTable">
+                  <thead>
+                    <tr>
+                      <th scope="col">S.no</th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">Date</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Action</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {filterTask?.map((t, i) => (
+                      <tr className="tableDataSetCursor" key={t._id}>
+                        <th scope="row">{i + 1}</th>
+                        <td onClick={() => handleDetails(t)}>{t?.title}</td>
+                        <td onClick={() => handleDetails(t)}>
+                          {t?.description.substring(0, 30)} read more..
+                        </td>
+                        <td>{new Date(t?.createdAt).toDateString()}</td>
+                        <td>
+                          {" "}
+                          <Checkbox
+                            {...label}
+                            checked={t.completed}
+                            value={t.completed}
+                            color="success"
+                            onClick={() => handleUpdateStatus(t)}
+                          />
+                        </td>
+                        <td>
+                          <button
+                            className="btn btn-danger"
+                            onClick={() => handleDelete(t._id)}
+                          >
+                            Delete
+                          </button>
+                          <button
+                            className="btn btn-success mx-2"
+                            onClick={() => handleUpdate(t)}
+                          >
+                            Update
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <AddTodo
-        open={open}
-        handleClose={handleClose}
-        todoUpdate={todoUpdate}
-        setTodoUpdate={setTodoUpdate}
-      />
-    </section>
+        <AddTodo
+          open={open}
+          handleClose={handleClose}
+          todoUpdate={todoUpdate}
+          setTodoUpdate={setTodoUpdate}
+        />
+      </section>
+    </>
   );
 };
 
---
+++ src/Components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <section className="loaderSet">
+      <div class="spinner-border text-danger atCenter" role="status">
+        <span class="sr-only"></span>
+      </div>
+    </section>
+  );
+};
+
+export default Loader;
